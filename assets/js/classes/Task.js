5d3566e5c8d78f454129a911dd818a6e181030fd
Tom <xnosx@hotmail.fr>
2024-03-22 09:06:41 +0100

    Div placement adjustement in Task.js

--- assets/js/classes/Task.js
+++ assets/js/classes/Task.js
@@ -20,21 +20,21 @@ export class Task {
         this.categories = categories;
         this.deletionConfirmModalHtml = `
             <div class="modal fade" id="delete-task-modal${this.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">Delete a task</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Delete a task</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        Are you sure you want to delete this task?
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
+                        <button type="button" class="btn btn-danger" id="delete-task-confirm-btn${this.id}">Yes</button>
+                    </div>
+                    </div>
                 </div>
-                <div class="modal-body">
-                    Are you sure you want to delete this task?
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
-                    <button type="button" class="btn btn-danger" id="delete-task-confirm-btn${this.id}">Yes</button>
-                </div>
-                </div>
-            </div>
             </div>`;
         this.html = `
             <button id="task-${this.id}" type="button" class="list-group-item list-group-item-action d-flex justify-content-between" aria-current="true"  
